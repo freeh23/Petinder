3ecd350d347b301220bd247f629318d1affc41fe
Free Hoebeke <freehoebeke@gmail.com>
2021-12-01 14:17:10 +0100

    codelab02

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import {RouterModule} from "@angular/router";
+import { ProfileGalleryComponent } from './profile-gallery/profile-gallery.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    ProfileGalleryComponent
+  ],
+  exports: [
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutModule { }
